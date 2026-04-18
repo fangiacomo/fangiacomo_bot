af88e6fb7eda3e23e28777c054e7abedff79219b
fangiacomo <sttfzs2v5b@privaterelay.appleid.com>
2026-04-18 16:14:50 +0200

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,41 +1,26 @@
-import requests
+mport requests
 import os
-import time
+
+print("BOT PARTITO")
 
 TELEGRAM_TOKEN = os.getenv("TELEGRAM_TOKEN")
 CHAT_ID = os.getenv("CHAT_ID")
 
-print("BOT STARTING TEST...")
+print("TOKEN:", TELEGRAM_TOKEN)
+print("CHAT_ID:", CHAT_ID)
 
-def send_message():
+url = f"https://api.telegram.org/bot{TELEGRAM_TOKEN}/sendMessage"
 
-    print("TOKEN:", TELEGRAM_TOKEN)
-    print("CHAT_ID:", CHAT_ID)
+try:
+    r = requests.post(
+        url,
+        data={
+            "chat_id": CHAT_ID,
+            "text": "TEST BOT FUNZIONANTE"
+        }
+    )
 
-    url = f"https://api.telegram.org/bot{TELEGRAM_TOKEN}/sendMessage"
+    print("RESPONSE:", r.text)
 
-    try:
-        r = requests.post(
-            url,
-            data={
-                "chat_id": CHAT_ID,
-                "text": "TEST MESSAGGIO BOT"
-            },
-            timeout=10
-        )
-
-        print("RESPONSE:", r.text)
-
-    except Exception as e:
-        print("ERROR:", e)
-
-if __name__ == "__main__":
-
-    print("INVIO MESSAGGIO...")
-
-    send_message()
-
-    print("FINE TEST")
-
-    while True:
-        time.sleep(60)
+except Exception as e:
+    print("ERROR:", e)
